4e837aa946b97053a0b0b584e5b6fcba10f873dd
unknown <ravindra@mantravat.com>
2019-02-13 11:46:00 +0530

    chang to mobile views

--- src/app/services/signup.service.ts
+++ src/app/services/signup.service.ts
@@ -9,11 +9,11 @@ export class SignupService {
 
   constructor(private http : HttpClient,private _HeaderService : HeaderService) { }
   signup(body){
-    console.log(body +"service methos")
+    console.log(body +"axjsajdfkdsjckzcjsjnservice methos")
     console.log(body);
      const header =this._HeaderService.config_Header(body);
      const headers = new HttpHeaders().set("X-CustomHttpHeader", "CUSTOM_VALUE");
-      return   this.http.get('http://192.168.0.11:3005/api/leave/101',{headers}).pipe(map(getData => {
+      return   this.http.post('http://192.168.0.18:3005/api/leave/101',body,{headers}).pipe(map(getData => {
         console.log(getData);
       return getData;
      }))
