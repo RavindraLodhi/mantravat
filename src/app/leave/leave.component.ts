9b37d1a8f205fddebc97b0040821d1c96fb8af54
Aditya <aditya@mantravat.com>
2019-02-26 20:09:46 +0530

    temo changes before rebase

--- src/app/leave/leave.component.ts
+++ src/app/leave/leave.component.ts
@@ -190,4 +190,23 @@ callComponentMethodHere(value){
   }
  
 }
+myFunctionFeedBack() {
+  console.log("runncsadsff");
+  
+  var x = document.getElementById("Feed");
+  console.log(x)
+  if (x.className === "col-sm-3  right-sidebar") {
+     x.className = "right-sidebarB";
+  } else {
+     x.className = "col-sm-3  right-sidebar";
+  }
+}
+myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "col-sm-2 sidebarleft") {
+    x.className = "sidebarleftB";
+  } else {
+    x.className = "col-sm-2 sidebarleft";
+  }
+}
 }
--- src/app/services/signup.service.ts
+++ src/app/services/signup.service.ts
@@ -9,11 +9,13 @@ export class SignupService {
 
   constructor(private http : HttpClient,private _HeaderService : HeaderService) { }
   signup(body){
-    console.log(body +"axjsajdfkdsjckzcjsjnservice methos")
+    console.log(body);
+    
+    console.log("signup service");
     console.log(body);
      const header =this._HeaderService.config_Header(body);
      const headers = new HttpHeaders().set("X-CustomHttpHeader", "CUSTOM_VALUE");
-      return   this.http.post('http://192.168.0.18:3005/api/leave/101',body,{headers}).pipe(map(getData => {
+      return   this.http.post('http://192.168.0.10:3005/api/signup',body,{headers}).pipe(map(getData => {
         console.log(getData);
       return getData;
      }))
