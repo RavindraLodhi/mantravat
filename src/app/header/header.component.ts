38f70857c2d5e69dd9f9352bb1f5e48c0b781f45
Aditya <aditya@mantravat.com>
2019-03-14 18:45:56 +0530

    Address screen tried

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,22 +12,7 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
 
-    var modal = document.getElementById('d');
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
-    var modal1 = document.getElementById('d1');
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-      if (event.target == modal1) {
-        modal1.style.display = "none";
-      }
-    }
+    
 
 
 
